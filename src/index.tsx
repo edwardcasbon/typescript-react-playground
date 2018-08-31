1274633dd414710b8fd8188dcb2de7797c995260
Edward Casbon <edward@edwardcasbon.co.uk>
2018-08-31 15:21:41 +0100

    Removed old button reference.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
 import { App } from "./app";
-
-import { Button } from "honeycomb-react/dist/components/Button";
 
 ReactDOM.render(<App/>, document.getElementById("app"));
